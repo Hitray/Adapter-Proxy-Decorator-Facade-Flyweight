c2b248e11723f4916735dc1ac05ec41fb6dc0204
Hitray <75577433+Hitray@users.noreply.github.com>
2022-10-17 07:46:32 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Ints intsCalc = new IntsCalculator();
-        System.out.println(intsCalc.sum(22, 33));
-        System.out.println(intsCalc.mult(10, 22));
+        System.out.println(intsCalc.sum(3, 3));
+        System.out.println(intsCalc.sum(15, 17));
         System.out.println(intsCalc.pow(2, 10));
+        System.out.println(intsCalc.mult(5, 5));
     }
+
+
 }
