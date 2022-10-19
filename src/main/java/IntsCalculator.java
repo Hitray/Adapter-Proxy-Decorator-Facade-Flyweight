01e62a6ab186a2668aa86d21babb97f435ceb3b5
Hitray <75577433+Hitray@users.noreply.github.com>
2022-10-19 21:49:11 +0500

    Update IntsCalculator.java

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -5,6 +5,7 @@ public class IntsCalculator implements Ints {
         this.target = new Calculator();
     }
 
+
     @Override
     public int sum(int arg0, int arg1) {
         return (int) target.newFormula()
@@ -15,12 +16,29 @@ public class IntsCalculator implements Ints {
     }
 
     @Override
-    public int mult(int arg0, int arg1) {
+    public int sub(int arg0, int arg1) {
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUB)
+                .result();
+    }
 
+    @Override
+    public int mult(int arg0, int arg1) {
         return (int) target.newFormula()
                 .addOperand(arg0)
                 .addOperand(arg1)
                 .calculate(Calculator.Operation.MULT)
+                .result();
+    }
+
+    @Override
+    public int div(int arg0, int arg1) {
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.DIV)
                 .result();
     }
 
